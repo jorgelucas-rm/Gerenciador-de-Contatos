7880a78d51ab09648792fbdee104555ea6d98271
jorgelucas-rm <jorgelucas.rm@outlook.com>
2024-05-24 14:44:53 -0300

    Atualização final para a apresentação

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,60 @@
-import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Agenda agenda = new Agenda();
+        Agenda agenda = null;
         int opcao = -1;
+        while (opcao != 6) {
 
-        while (opcao != 5) {
-
-            System.out.println("\nQual operação você deseja realizar?\n\n" +
-                    "[1] Adicionar um contato\n" +
-                    "[2] Remover um contato\n" +
-                    "[3] Buscar um contato\n" +
-                    "[4] Listar contatos\n" +
-                    "[5] Sair\n");
+            System.out.println(
+                    "\nQual operação você deseja realizar?\n\n" +
+                            "[1] Adicionar um contato\n" +
+                            "[2] Remover um contato\n" +
+                            "[3] Atualizar um contato\n" +
+                            "[4] Buscar um contato\n" +
+                            "[5] Listar contatos\n" +
+                            "[6] Sair\n"
+            );
 
             Scanner scan = new Scanner(System.in);
             System.out.println("Opção desejada:");
             opcao = scan.nextInt();
 
+            while (1 < opcao && opcao > 6) {
+                System.out.println("\nOpção inválida\n");
+                System.out.println("Opção desejada:");
+                opcao = scan.nextInt();
+                scan.nextLine();
+            }
+
             switch (opcao) {
                 case 1:
+                    agenda = new Agenda();
                     agenda.adicionarContato();
                     break;
 
                 case 2:
+                    agenda = new Agenda();
                     agenda.removerContato();
                     break;
 
                 case 3:
-                    agenda.buscarContato();
+                    agenda = new Agenda();
+                    agenda.atualizarContato();
                     break;
 
                 case 4:
-                    agenda.listarContatos();
+                    agenda = new Agenda();
+                    agenda.buscarContato();
                     break;
 
                 case 5:
+                    agenda = new Agenda();
+                    agenda.listarContatos();
+                    break;
+
+                case 6:
+                    agenda = new Agenda();
                     scan.close();
                     break;
             }
